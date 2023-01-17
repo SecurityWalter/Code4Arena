433adfc52e0201e909c31e9c69974e99fb3ca6f1
SecurityWalter <122684946+SecurityWalter@users.noreply.github.com>
2023-01-17 16:59:30 +0100

    update

---
+++ Ondo Finance/requestMint.sol
@@ -0,0 +1,36 @@
+  function requestMint(
+    uint256 collateralAmountIn
+  )
+    external
+    override
+    updateEpoch
+    nonReentrant
+    whenNotPaused
+    checkKYC(msg.sender)
+  {
+    if (collateralAmountIn < minimumDepositAmount) {
+      revert MintRequestAmountTooSmall();
+    }
+
+    uint256 feesInCollateral = _getMintFees(collateralAmountIn);
+    uint256 depositValueAfterFees = collateralAmountIn - feesInCollateral;
+
+
+    collateral.safeTransferFrom(msg.sender, feeRecipient, feesInCollateral);
+    collateral.safeTransferFrom(
+      msg.sender,
+      assetRecipient,
+      depositValueAfterFees
+    );
+        _checkAndUpdateMintLimit(depositValueAfterFees);
+
+    mintRequestsPerEpoch[currentEpoch][msg.sender] += depositValueAfterFees;
+
+    emit MintRequested(
+      msg.sender,
+      currentEpoch,
+      collateralAmountIn,
+      depositValueAfterFees,
+      feesInCollateral
+    );
+  }
